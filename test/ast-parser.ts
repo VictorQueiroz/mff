8b30123a4e963290d38dcc415fa6ffb793049572
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-29 08:26:03 -0300

    test/ast-parser.ts: create unit test for ASTParser

---
+++ test/ast-parser.ts
@@ -0,0 +1,62 @@
+import ASTParser from '../src/ast-parser';
+import { Container } from '../src/ast-parser/constants';
+import { Params } from '../src/ast-parser/param';
+import * as assert from 'assert';
+
+function parse(text: string) {
+    const containers: Container[] = [];
+
+    new ASTParser({
+        text
+    }, {
+        directory: '',
+        containers
+    }).parse();
+
+    return containers;
+}
+
+export default function() {
+    return {
+        'it should look for references inside namespaces': function() {
+            assert.deepEqual(parse(`
+                namespace api {
+                    type TSetConfig {
+                        SetConfig -> TConfig config
+                    }
+                    type TConfig {
+                        config -> string key, string value
+                    }
+                }
+            `), [{
+                id: 3561931015,
+                type: 'api::TSetConfig',
+                params: [{
+                    name: 'config',
+                    type: {
+                        type: Params.Reference,
+                        containers: ['api::config']
+                    }
+                }],
+                name: 'api::SetConfig'
+            }, {
+                id: 2472372924,
+                type: 'api::TConfig',
+                name: 'api::config',
+                params: [{
+                    name: 'key',
+                    type: {
+                        type: Params.Generic,
+                        name: 'string'
+                    }
+                }, {
+                    name: 'value',
+                    type: {
+                        type: Params.Generic,
+                        name: 'string'
+                    }
+                }]
+            }]);
+        }
+    };
+};
