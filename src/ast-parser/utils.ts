d4a85ebb914762085add5e0403f7075c54e17f37
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-29 21:30:00 -0300

    create utils file to check if type is a generic

---
+++ src/ast-parser/utils.ts
@@ -0,0 +1,20 @@
+import { Generics } from './constants';
+
+export function isGeneric(type: string) {
+    switch(type) {
+        case Generics.String:
+        case Generics.Float:
+        case Generics.Double:
+        case Generics.Int8:
+        case Generics.UInt8:
+        case Generics.Boolean:
+        case Generics.Int32:
+        case Generics.Int64:
+        case Generics.Int16:
+        case Generics.UInt16:
+        case Generics.UInt32:
+        case Generics.UInt64:
+            return true;
+    }
+    return false;
+}
