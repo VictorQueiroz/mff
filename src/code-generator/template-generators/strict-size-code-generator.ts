6a78086756084d94a49515ec36c8889a3dae60ee
Victor Queiroz <victorcqueirozg@gmail.com>
2019-03-11 23:01:24 -0300

    update strict size code generator

--- src/code-generator/template-generators/strict-size-code-generator.ts
+++ src/code-generator/template-generators/strict-size-code-generator.ts
@@ -1,20 +1,43 @@
 import TypeCodeGenerator, { ITypeCodecOptions } from "../type-code-generator";
-import { NodeContainerParam } from "../../ast-parser/node";
+import { NodeContainerParam, NodeTemplate } from "../../ast-parser/node";
 import ContainerDeclarationGenerator from "../container-declaration-generator";
 import { Syntax, Generics } from "../../ast-parser/constants";
 
 export default class StrictSizeCodeGenerator extends TypeCodeGenerator<NodeContainerParam> {
     public getDecodingCode(node: NodeContainerParam, options: ITypeCodecOptions) {
-        const {valueOf, append, write} = this.cs;
+        const {valueOf, append} = this.cs;
         const {paramType} = node;
         if(paramType.type !== Syntax.Template) {
             throw new Error('Received invalid param type');
         }
-        const [firstArgument, secondArgument] = paramType.arguments;
+        const [firstArgument] = paramType.arguments;
         append(new ContainerDeclarationGenerator(this).createParamDecodingStatement({
             ...node,
             paramType: firstArgument
         }, options));
+        this.createValidationCode(node, paramType, options);
+        return valueOf();
+    }
+    public getEncodingCode(node: NodeContainerParam, options: ITypeCodecOptions) {
+        const {valueOf, append} = this.cs;
+        const {paramType} = node;
+        if(paramType.type !== Syntax.Template) {
+            throw new Error('Invalid param type');
+        }
+        this.createValidationCode(node, paramType, options);
+        append(new ContainerDeclarationGenerator(this).createParamEncodingStatement({
+            ...node,
+            paramType: paramType.arguments[0]
+        }, options));
+        return valueOf();
+    }
+    private createValidationCode(
+        node: NodeContainerParam,
+        paramType: NodeTemplate,
+        options: ITypeCodecOptions
+    ) {
+        const {write} = this.cs;
+        const [firstArgument, secondArgument] = paramType.arguments;
         if(secondArgument.type !== Syntax.LiteralNumber) {
             throw new Error('Second argument of StrictSize must be of type number');
         }
@@ -36,18 +59,5 @@ export default class StrictSizeCodeGenerator extends TypeCodeGenerator<NodeConta
                 `throw new Error(\`Expected ${size} but got \${valueVariable.length} instead\`);\n`
             );
         }, '}\n');
-        return valueOf();
-    }
-    public getEncodingCode(node: NodeContainerParam, options: ITypeCodecOptions) {
-        const {valueOf, append} = this.cs;
-        const {paramType} = node;
-        if(paramType.type !== Syntax.Template) {
-            throw new Error('Invalid param type');
-        }
-        append(new ContainerDeclarationGenerator(this).createParamEncodingStatement({
-            ...node,
-            paramType: paramType.arguments[0]
-        }, options));
-        return valueOf();
     }
 }
