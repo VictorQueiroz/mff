e45207e1af4b4ef00374361223d6e4c89d015f45
Victor Queiroz <victorcqueirozg@gmail.com>
2019-03-17 12:11:07 -0300

    scripts/generate-schemas.ts: update code generators to generate interfaces only TS code

--- scripts/generate-schemas.ts
+++ scripts/generate-schemas.ts
@@ -5,6 +5,9 @@ import * as fs from 'fs';
 import ASTPreprocessor from '../src/ast-parser/ast-preprocessor';
 import { CodeGenerator } from '../src/code-generator';
 import { format } from 'util';
+import EmptyCodeGenerator from '../src/code-generator/empty-code-generator';
+import ContainerDeclarationInterfaceGenerator from '../src/code-generator/container-declaration-interface-generator';
+import ContainerGroupInterfaceGenerator from '../src/code-generator/container-group-interface-generator';
 
 async function run(): Promise<number> {
     const nodes = btc.parse(fs.readFileSync(path.resolve(__dirname, '../test/schema.txt'), 'utf8'));
@@ -17,6 +20,15 @@ async function run(): Promise<number> {
     const contents = Buffer.from(cg.generate(), 'utf8');
 
     fs.writeFileSync(path.resolve(__dirname, '../test/schema.ts'), contents);
+
+    cg.generators.set('utilClass', new EmptyCodeGenerator(cg));
+    cg.generators.set('dataContainer', new EmptyCodeGenerator(cg));
+    cg.generators.set('importsDeclaration', new EmptyCodeGenerator(cg));
+    cg.generators.set('containerDeclaration', new ContainerDeclarationInterfaceGenerator(cg));
+    cg.generators.set('containerGroup', new ContainerGroupInterfaceGenerator(cg));
+
+    fs.writeFileSync(path.resolve(__dirname, '../test/schema-interfaces.ts'), cg.generate());
+
     return contents.byteLength;
 }
 
