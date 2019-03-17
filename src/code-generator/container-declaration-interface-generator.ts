4bb1ad12c1ce3003f46e186d75d2679ed611f650
Victor Queiroz <victorcqueirozg@gmail.com>
2019-03-17 12:23:29 -0300

    ContainerDeclarationInterfaceGenerator: allow interfaces not to be readonly but make them be by default

--- src/code-generator/container-declaration-interface-generator.ts
+++ src/code-generator/container-declaration-interface-generator.ts
@@ -1,7 +1,20 @@
 import { NodeContainerDeclaration } from '../ast-parser/node';
 import ContainerDeclarationGenerator from './container-declaration-generator';
+import { ICodeGenerator } from './code-generator';
+
+export interface IContainerInterfaceOptions {
+    readonly?: boolean;
+}
 
 export default class ContainerDeclarationInterfaceGenerator extends ContainerDeclarationGenerator {
+    private options: IContainerInterfaceOptions;
+    constructor(parent: ICodeGenerator, options?: IContainerInterfaceOptions) {
+        super(parent);
+        this.options = {
+            readonly: true,
+            ...options
+        };
+    }
     public generate(node: NodeContainerDeclaration) {
         const {write, append, valueOf} = this.cs;
         this.setCurrentNode(node);
@@ -9,13 +22,26 @@ export default class ContainerDeclarationInterfaceGenerator extends ContainerDec
             this.attachComments(node.leadingComments);
         }
         const container = this.getContainer(node.name);
+        const paramPrefix = this.getInterfaceParamPrefix();
         write(`interface ${this.getClassNameFromList(node)} {\n`, () => {
-            write(`readonly _id?: 0x${container.id.toString(16)};\n`);
-            write(`readonly _type?: "${container.type}";\n`);
-            write(`readonly _name: "${container.name}";\n`);
-            append(this.createContainerInterfaceParams(node, (param) => `readonly ${param}`));
+            write(`${paramPrefix}_id?: 0x${container.id.toString(16)};\n`);
+            write(`${paramPrefix}_type?: "${container.type}";\n`);
+            write(`${paramPrefix}_name: "${container.name}";\n`);
+            append(this.createContainerInterfaceParams(node, (param) => `${paramPrefix}${param}`));
         }, '}\n');
         this.setCurrentNode(undefined);
         return valueOf();
+    }
+    public getInterfaceParamPrefix() {
+        let text = '';
+        const {readonly} = this.options;
+        if(readonly) {
+            text += 'readonly';
+        }
+
+        if(text.length > 0) {
+            text += ' ';
+        }
+        return text;
     }
 }
