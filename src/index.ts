a31949dcf24b3164692ad52f33a37038c2e960bf
Victor Queiroz <victorcqueirozg@gmail.com>
2020-05-30 14:11:18 -0300

    Update export declaration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as Schema } from './schema';
 export { default as ASTParser } from './ast-parser';
-export { default as CodeGenerator } from './code-generator';
+export { CodeGenerator } from './code-generator';
 export { default as btc } from './btc';
 export { default as Serializer } from './schema/serializer';
 export { default as Deserializer } from './schema/deserializer';
